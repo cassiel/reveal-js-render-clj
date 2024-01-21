4f50d220e0daa5348c521c062707c62ad9d99fc5
Nick (Git client) <nick@cassiel.com>
2024-01-21 15:03:00 +0000

    Port change - though we don't really need the Docker SSI server at all.

--- server.sh
+++ server.sh
@@ -2,6 +2,6 @@
 
 # Run up an SSH-enabled Apache in the current directory.
 
-docker run --rm --name httpd-ssi -p 5000:80 \
+docker run --rm --name httpd-ssi -p 5050:80 \
        -v "$PWD":/usr/local/apache2/htdocs/ \
        cassiel/httpd-ssi
